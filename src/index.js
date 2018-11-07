f9ebd03a63578d1f7bad98e10ba76eaedc229ed4
Raffaele Cigni <rcigni@byte-code.com>
2018-11-07 17:27:39 +0100

    routing to / and about

--- src/index.js
+++ src/index.js
@@ -4,16 +4,24 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers/reducers';
 import GameContainers from './containers/GameContainers'
-  
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 
+const About = () => <h2>About</h2>;
+
 ReactDOM.render(
   <Provider store ={store}>
-    <GameContainers />
+      <Router>
+          <div>
+            <Route exact path="/" component={GameContainers} />
+            <Route path="/about" component={About} />
+          </div>
+      </Router>
   </Provider>, 
   document.getElementById("root")
 );
